e066b7f6adeb75313ef5788e2eebf530ffa58512
Angger Muhammad ELegant <anggerelegant7@gmail.com>
2022-11-15 17:06:16 +0700

    sql varchar to text

--- lawencon_community.sql
+++ lawencon_community.sql
@@ -1,10 +1,10 @@
 CREATE TABLE t_file(
-	id varchar(36),
+	id text,
 	file_codes text not null,
-	extensions varchar(5) not null,
-	created_by varchar(36) not null,
+	extensions text not null,
+	created_by text not null,
 	created_at timestamp without time zone not null,
-	updated_by varchar(36),
+	updated_by text,
 	updated_at timestamp without time zone,
 	ver int not null default 0,
 	is_active boolean not null default true
@@ -12,12 +12,12 @@ CREATE TABLE t_file(
 ALTER TABLE t_file ADD CONSTRAINT t_file_pk PRIMARY KEY(id);
 
 CREATE TABLE t_role (
-    id varchar(36),
-    role_code varchar(5) NOT NULL,
-    role_name varchar(20) NOT NULL,
-    created_by varchar(36) NOT NULL,
+    id text,
+    role_code text NOT NULL,
+    role_name text NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -27,12 +27,12 @@ ALTER TABLE t_role ADD CONSTRAINT t_role_bk UNIQUE(role_code);
 ALTER TABLE t_role ADD CONSTRAINT t_role_ck UNIQUE(role_code, role_name);
 
 CREATE TABLE t_post_type(
-    id varchar(36),
-    post_type_name varchar(20) NOT NULL,
-    post_type_code varchar(5) NOT NULL,
-    created_by varchar(36) NOT NULL,
+    id text,
+    post_type_name text NOT NULL,
+    post_type_code text NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -42,12 +42,12 @@ ALTER TABLE t_post_type ADD CONSTRAINT t_post_type_bk UNIQUE(post_type_code);
 ALTER TABLE t_post_type ADD CONSTRAINT t_post_type_ck UNIQUE(post_type_code, post_type_name);
 
 CREATE TABLE t_industry (
-    id varchar(36),
-    industry_name varchar(50) NOT NULL,
-    industry_code varchar(5) NOT NULL,
-    created_by varchar(36) NOT NULL,
+    id text,
+    industry_name text NOT NULL,
+    industry_code text NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -57,12 +57,12 @@ ALTER TABLE t_industry ADD CONSTRAINT t_industry_bk UNIQUE(industry_code);
 ALTER TABLE t_industry ADD CONSTRAINT t_industry_ck UNIQUE(industry_code, industry_name);
 
 CREATE TABLE t_position(
-    id varchar(36),
-    position_name varchar(50) NOT NULL,
-    position_code varchar(5) NOT NULL,
-    created_by varchar(36) NOT NULL,
+    id text,
+    position_name text NOT NULL,
+    position_code text NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -72,20 +72,20 @@ ALTER TABLE t_position ADD CONSTRAINT t_position_bk UNIQUE(position_code);
 ALTER TABLE t_position ADD CONSTRAINT t_position_ck UNIQUE(position_code, position_name);
 
 CREATE TABLE t_user(
-    id varchar(36),
-    fullname varchar(50) NOT NULL,
-    email varchar(50) NOT NULL,
+    id text,
+    fullname text NOT NULL,
+    email text NOT NULL,
     pass text NOT NULL,
-    company varchar(50) NOT NULL,
+    company text NOT NULL,
     wallet double precision NOT NULL DEFAULT 0,
     is_premium boolean NOT NULL DEFAULT FALSE,
-    role_id varchar(36) NOT NULL,
-    industry_id varchar(36) NOT NULL,
-    position_id varchar(36) NOT NULL,
-    file_id varchar(36) NOT NULL,
-    created_by varchar(36) NOT NULL,
+    role_id text NOT NULL,
+    industry_id text NOT NULL,
+    position_id text NOT NULL,
+    file_id text NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -99,12 +99,12 @@ ALTER TABLE t_user ADD CONSTRAINT t_user_position_fk FOREIGN KEY (position_id) R
 ALTER TABLE t_user ADD CONSTRAINT t_user_file_fk FOREIGN KEY (file_id) REFERENCES t_file(id);
 
 CREATE TABLE t_article(
-    id varchar(36),
-    article_title varchar(100) NOT NULL,
+    id text,
+    article_title text NOT NULL,
     article_content text NOT NULL,
-    created_by varchar(36) NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -112,12 +112,12 @@ CREATE TABLE t_article(
 ALTER TABLE t_article ADD CONSTRAINT t_article_pk PRIMARY KEY(id);
 
 CREATE TABLE t_post(
-    id varchar(36),
-    post_title varchar(100) NOT NULL,
+    id text,
+    post_title text NOT NULL,
     post_content text NOT NULL,
-    created_by varchar(36) NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -125,12 +125,12 @@ CREATE TABLE t_post(
 ALTER TABLE t_post ADD CONSTRAINT t_post_pk PRIMARY KEY(id);
 
 CREATE TABLE t_bookmark(
-    id varchar(36),
-    user_id varchar(36) NOT NULL,
-    post_id varchar(36) NOT NULL,
-    created_by varchar(36) NOT NULL,
+    id text,
+    user_id text NOT NULL,
+    post_id text NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -140,12 +140,12 @@ ALTER TABLE t_bookmark ADD CONSTRAINT t_bookmark_user_fk FOREIGN KEY(user_id) RE
 ALTER TABLE t_bookmark ADD CONSTRAINT t_bookmark_post_fk FOREIGN KEY(post_id) REFERENCES t_post(id);
 
 CREATE TABLE t_like(
-    id varchar(36),
-    user_id varchar(36) NOT NULL,
-    post_id varchar(36) NOT NULL,
-    created_by varchar(36) NOT NULL,
+    id text,
+    user_id text NOT NULL,
+    post_id text NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -155,14 +155,14 @@ ALTER TABLE t_like ADD CONSTRAINT t_like_user_fk FOREIGN KEY(user_id) REFERENCES
 ALTER TABLE t_like ADD CONSTRAINT t_like_post_fk FOREIGN KEY(post_id) REFERENCES t_post(id);
 
 CREATE TABLE t_comment(
-    id varchar(36),
+    id text,
     comment_content text NOT NULL,
-    user_id varchar(36) NOT NULL,
-    post_id varchar(36),
-    comment_id varchar(36),
-    created_by varchar(36) NOT NULL,
+    user_id text NOT NULL,
+    post_id text,
+    comment_id text,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -173,12 +173,12 @@ ALTER TABLE t_comment ADD CONSTRAINT t_comment_post_fk FOREIGN KEY(post_id) REFE
 ALTER TABLE t_comment ADD CONSTRAINT t_comment_comment_fk FOREIGN KEY(comment_id) REFERENCES t_comment(id);
 
 CREATE TABLE t_attachment_post(
-    id varchar(36),
-    file_id varchar(36) NOT NULL,
-    post_id varchar(36) NOT NULL,
-    created_by varchar(36) NOT NULL,
+    id text,
+    file_id text NOT NULL,
+    post_id text NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -188,12 +188,12 @@ ALTER TABLE t_attachment_post ADD CONSTRAINT t_attachment_post_file_fk FOREIGN K
 ALTER TABLE t_attachment_post ADD CONSTRAINT t_attachment_post_post_fk FOREIGN KEY(post_id) REFERENCES t_post(id);
 
 CREATE TABLE t_attachment_article(
-    id varchar(36),
-    file_id varchar(36) NOT NULL,
-    article_id varchar(36) NOT NULL,
-    created_by varchar(36) NOT NULL,
+    id text,
+    file_id text NOT NULL,
+    article_id text NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -203,13 +203,13 @@ ALTER TABLE t_attachment_article ADD CONSTRAINT t_attachment_article_file_fk FOR
 ALTER TABLE t_attachment_article ADD CONSTRAINT t_attachment_article_article_fk FOREIGN KEY(article_id) REFERENCES t_article(id);
 
 CREATE TABLE t_poll (
-    id varchar(36),
-    poll_title varchar(100) NOT NULL,
+    id text,
+    poll_title text NOT NULL,
     end_at timestamp WITHOUT time ZONE NOT NULL,
-    post_id varchar(36) NOT NULL,
-    created_by varchar(36) NOT NULL,
+    post_id text NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -218,12 +218,12 @@ ALTER TABLE t_poll ADD CONSTRAINT t_poll_pk PRIMARY KEY(id);
 ALTER TABLE t_poll ADD CONSTRAINT t_poll_post_fk FOREIGN KEY(post_id) REFERENCES t_post(id);
 
 CREATE TABLE t_poll_option(
-    id varchar(36),
+    id text,
     poll_content text NOT NULL,
-    post_id varchar(36) NOT NULL,
-    created_by varchar(36) NOT NULL,
+    post_id text NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -232,12 +232,12 @@ ALTER TABLE t_poll_option ADD CONSTRAINT t_poll_option_pk PRIMARY KEY(id);
 ALTER TABLE t_poll_option ADD CONSTRAINT t_poll_option_post_fk FOREIGN KEY(post_id) REFERENCES t_post(id);
 
 CREATE TABLE t_poll_vote (
-    id varchar(36),
-    poll_option_id varchar(36) NOT NULL,
-    user_id varchar(36) NOT NULL,
-    created_by varchar(36) NOT NULL,
+    id text,
+    poll_option_id text NOT NULL,
+    user_id text NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -247,12 +247,12 @@ ALTER TABLE t_poll_vote ADD CONSTRAINT t_poll_vote_option_fk FOREIGN KEY(poll_op
 ALTER TABLE t_poll_vote ADD CONSTRAINT t_poll_vote_user_fk FOREIGN KEY(user_id) REFERENCES t_user(id);
 
 CREATE TABLE t_activity_type(
-    id varchar(36),
-    activity_type_name varchar(20) NOT NULL,
-    activity_type_code varchar(6) NOT NULL,
-    created_by varchar(36) NOT NULL,
+    id text,
+    activity_type_name text NOT NULL,
+    activity_type_code text NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -262,17 +262,17 @@ ALTER TABLE t_activity_type ADD CONSTRAINT t_activity_type_bk UNIQUE(activity_ty
 ALTER TABLE t_activity_type ADD CONSTRAINT t_activity_type_ck UNIQUE(activity_type_code, activity_type_name);
 
 CREATE TABLE t_activity(
-    id varchar(36),
-    title varchar(50) NOT NULL,
-    provider varchar(50) NOT NULL,
+    id text,
+    title text NOT NULL,
+    provider text NOT NULL,
     activity_location text NOT NULL,
     start_at timestamp WITHOUT time ZONE NOT NULL,
     end_at timestamp WITHOUT time ZONE NOT NULL,
     fee double precision NOT NULL,
-    activity_type_id varchar(36) NOT NULL,
-    created_by varchar(36) NOT NULL,
+    activity_type_id text NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -281,12 +281,12 @@ ALTER TABLE t_activity ADD CONSTRAINT t_activity_pk PRIMARY KEY(id);
 ALTER TABLE t_activity ADD CONSTRAINT t_activity_activity_type_fk FOREIGN KEY(activity_type_id) REFERENCES t_activity_type(id);
 
 CREATE TABLE t_attachment_activity(
-    id varchar(36),
-    file_id varchar(36) NOT NULL,
-    activity_id varchar(36) NOT NULL,
-    created_by varchar(36) NOT NULL,
+    id text,
+    file_id text NOT NULL,
+    activity_id text NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -296,14 +296,14 @@ ALTER TABLE t_attachment_activity ADD CONSTRAINT t_attachment_activity_file_fk F
 ALTER TABLE t_attachment_activity ADD CONSTRAINT t_attachment_activity_post_fk FOREIGN KEY(activity_id) REFERENCES t_activity(id);
 
 CREATE TABLE t_activity_member (
-    id varchar(36),
+    id text,
     is_approved boolean NOT NULL DEFAULT FALSE,
-    file_id varchar(36) NOT NULL,
-    activity_id varchar(36) NOT NULL,
-    user_id varchar(36) NOT NULL,
-    created_by varchar(36) NOT NULL,
+    file_id text NOT NULL,
+    activity_id text NOT NULL,
+    user_id text NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -314,13 +314,13 @@ ALTER TABLE t_activity_member ADD CONSTRAINT t_activity_member_post_fk FOREIGN K
 ALTER TABLE t_activity_member ADD CONSTRAINT t_activity_member_user_fk FOREIGN KEY(user_id) REFERENCES t_user(id); 
 
 CREATE TABLE t_payment_activity(
-    id varchar(36),
+    id text,
     nominal double precision NOT NULL,
-    user_id varchar(36) NOT NULL,
-    activity_id varchar(36) NOT NULL,
-    created_by varchar(36) NOT NULL,
+    user_id text NOT NULL,
+    activity_id text NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
@@ -330,13 +330,13 @@ ALTER TABLE t_payment_activity ADD CONSTRAINT t_payment_activity_user_fk FOREIGN
 ALTER TABLE t_payment_activity ADD CONSTRAINT t_payment_activity_event_fk FOREIGN KEY(activity_id) REFERENCES t_activity(id);
 
 CREATE TABLE t_payment_premium(
-    id varchar(36),
+    id text,
     nominal double precision NOT NULL,
-    user_id varchar(36) NOT NULL,
-    activity_id varchar(36) NOT NULL,
-    created_by varchar(36) NOT NULL,
+    user_id text NOT NULL,
+    activity_id text NOT NULL,
+    created_by text NOT NULL,
     created_at timestamp WITHOUT time ZONE NOT NULL,
-    updated_by varchar(36),
+    updated_by text,
     updated_at timestamp WITHOUT time ZONE,
     ver int NOT NULL DEFAULT 0,
     is_active boolean NOT NULL DEFAULT TRUE
