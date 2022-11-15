853d97d8f8efb72d0a7c81aa20fc755b8c65ed44
Muhammad Nur Fauzi <fauzi.ozi.mnf@gmail.com>
2022-11-15 09:22:10 +0700

    Added lombok plugin and dependencies

---
+++ lawencon-community/src/main/java/com/lawencon/community/model/PostType.java
@@ -0,0 +1,30 @@
+package com.lawencon.community.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import com.lawencon.base.BaseEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "t_post_type", uniqueConstraints = {
+		@UniqueConstraint(name = "t_post_type_bk", columnNames = { "post_type_code" }),
+		@UniqueConstraint(name = "t_post_type_ck", columnNames = { "post_type_code", "post_type_name" }) })
+public class PostType extends BaseEntity {
+
+	private static final long serialVersionUID = -4945398026799662087L;
+
+	@Column(name = "post_type_name", nullable = false, length = 20)
+	@Setter
+	@Getter
+	private String postTypeName;
+	@Column(name = "post_type_code", nullable = false, length = 5)
+	@Setter
+	@Getter
+	private String postTypeCode;
+
+}
