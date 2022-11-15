e6df3871437899dcbe73f523e7fbe594a59c3aae
Muhammad Nur Fauzi <fauzi.ozi.mnf@gmail.com>
2022-11-15 16:50:54 +0700

    Add role service and controller

--- lawencon-community/src/main/java/com/lawencon/community/dao/RoleDao.java
+++ lawencon-community/src/main/java/com/lawencon/community/dao/RoleDao.java
@@ -4,6 +4,6 @@ import org.springframework.stereotype.Repository;
 import com.lawencon.base.AbstractJpaDao;
 
 @Repository
-public class UserRoleDao extends AbstractJpaDao {
+public class RoleDao extends AbstractJpaDao {
 
 }
