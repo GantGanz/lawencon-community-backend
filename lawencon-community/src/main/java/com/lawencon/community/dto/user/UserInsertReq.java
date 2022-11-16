4285775fef2696b2ecec6708f4e57d14d87a557d
Muhammad Nur Fauzi <fauzi.ozi.mnf@gmail.com>
2022-11-16 14:52:55 +0700

    Add working register service and controller

--- lawencon-community/src/main/java/com/lawencon/community/dto/user/UserInsertReq.java
+++ lawencon-community/src/main/java/com/lawencon/community/dto/user/UserInsertReq.java
@@ -1,20 +1,18 @@
 package com.lawencon.community.dto.user;
 
-import java.math.BigDecimal;
-
 import lombok.Data;
 
 @Data
 public class UserInsertReq {
-	
 	private String fullname;
 	private String email;
+	private String password;
 	private String company;
 	private String fileCodes;
 	private String extension;
 	private String roleId;
 	private String positionId;
-	private BigDecimal wallet;
+	private String industryId;
 	private Boolean isPremium;
-	
+
 }
--- lawencon-community/src/main/java/com/lawencon/community/service/UserService.java
+++ lawencon-community/src/main/java/com/lawencon/community/service/UserService.java
@@ -2,28 +2,140 @@ package com.lawencon.community.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.lawencon.base.BaseCoreService;
+import com.lawencon.community.dao.FileDao;
+import com.lawencon.community.dao.IndustryDao;
+import com.lawencon.community.dao.PositionDao;
+import com.lawencon.community.dao.RoleDao;
 import com.lawencon.community.dao.UserDao;
+import com.lawencon.community.dto.InsertDataRes;
+import com.lawencon.community.dto.InsertRes;
 import com.lawencon.community.dto.user.UserData;
+import com.lawencon.community.dto.user.UserInsertReq;
 import com.lawencon.community.dto.user.UsersRes;
+import com.lawencon.community.model.File;
+import com.lawencon.community.model.Industry;
+import com.lawencon.community.model.Position;
+import com.lawencon.community.model.Role;
 import com.lawencon.community.model.User;
 
 @Service
 public class UserService extends BaseCoreService implements UserDetailsService {
 	@Autowired
 	private UserDao userDao;
+	@Autowired
+	private RoleDao roleDao;
+	@Autowired
+	private PositionDao positionDao;
+	@Autowired
+	private IndustryDao industryDao;
+	@Autowired
+	private FileDao fileDao;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public InsertRes register(final UserInsertReq data) {
+		valInsert(data);
+
+		User userInsert = new User();
+		userInsert.setFullname(data.getFullname());
+		userInsert.setEmail(data.getEmail());
+
+		final String hashPassword = passwordEncoder.encode(data.getPassword());
+		userInsert.setPass(hashPassword);
+
+		userInsert.setCompany(data.getCompany());
+		userInsert.setIsPremium(data.getIsPremium());
+
+		final Role role = roleDao.getById(Role.class, data.getRoleId());
+		userInsert.setRole(role);
+
+		final Industry industry = industryDao.getById(Industry.class, data.getIndustryId());
+		userInsert.setIndustry(industry);
+
+		final Position position = positionDao.getById(Position.class, data.getPositionId());
+		userInsert.setPosition(position);
+
+		final File file = new File();
+		file.setFileCodes(data.getFileCodes());
+		file.setExtensions(data.getExtension());
+
+		try {
+			begin();
+
+			final File fileInsert = fileDao.saveNoLogin(file, () -> userDao.getSystemId());
+			userInsert.setFile(fileInsert);
+
+			userInsert = userDao.saveNoLogin(userInsert, () -> userDao.getSystemId());
+			commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			rollback();
+		}
+
+		final InsertDataRes dataRes = new InsertDataRes();
+		dataRes.setId(userInsert.getId());
+
+		final InsertRes insertRes = new InsertRes();
+		insertRes.setData(dataRes);
+		insertRes.setMessage("Registration Success");
+
+		return insertRes;
+	}
+
+	private void valInsert(final UserInsertReq data) {
+		bkNotDuplicate(data);
+		idNull(data);
+		fkFound(data);
+	}
+
+	private void idNull(final UserInsertReq data) {
+		if (data.getRoleId() == null) {
+			throw new RuntimeException("Role id must not null");
+		}
+		if (data.getPositionId() == null) {
+			throw new RuntimeException("Position id must not null");
+		}
+		if (data.getIndustryId() == null) {
+			throw new RuntimeException("Industry id must not null");
+		}
+	}
+
+	private void fkFound(final UserInsertReq data) {
+		final Role role = roleDao.getByIdAndDetach(Role.class, data.getRoleId());
+		if (role == null) {
+			throw new RuntimeException("Role not found");
+		}
+		final Position position = positionDao.getByIdAndDetach(Position.class, data.getPositionId());
+		if (position == null) {
+			throw new RuntimeException("Position not found");
+		}
+		final Industry industry = industryDao.getByIdAndDetach(Industry.class, data.getIndustryId());
+		if (industry == null) {
+			throw new RuntimeException("Industry not found");
+		}
+	}
+
+	private void bkNotDuplicate(final UserInsertReq data) {
+		final Optional<User> userOptional = userDao.getByEmail(data.getEmail());
+		if (userOptional.isPresent()) {
+			throw new RuntimeException("Email already used");
+		}
 	}
 
 	public UsersRes getAll() {
