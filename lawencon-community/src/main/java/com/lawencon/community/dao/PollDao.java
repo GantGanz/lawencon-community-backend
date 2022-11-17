8ab634037470ad8ecc864aae62cca715d7699a44
Angger Muhammad ELegant <anggerelegant7@gmail.com>
2022-11-17 14:56:09 +0700

    add and fix poll and polloption dao, dto, and model

--- lawencon-community/src/main/java/com/lawencon/community/dao/PollDao.java
+++ lawencon-community/src/main/java/com/lawencon/community/dao/PollDao.java
@@ -1,9 +1,46 @@
 package com.lawencon.community.dao;
+import java.sql.Timestamp;
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
 import com.lawencon.base.AbstractJpaDao;
+import com.lawencon.community.model.Poll;
+import com.lawencon.community.model.Post;
 
 @Repository
 public class PollDao extends AbstractJpaDao {
 
+	public Optional<Poll> getByPostId(final String postId) {
+		final StringBuilder str = new StringBuilder();
+		str.append("SELECT p.id, p.poll_title, p.end_at, p.post_id, p.is_active ")
+				.append("FROM t_poll p ")
+				.append("WHERE p.post_id = :postId ")
+				.append("AND u.is_active = TRUE ");
+
+		Poll poll = null;
+		try {
+			final Object pollObj = createNativeQuery(str.toString()).setParameter("postId", postId).getSingleResult();
+
+			if (pollObj != null) {
+				final Object[] objArr = (Object[]) pollObj;
+				poll = new Poll();
+				poll.setId(objArr[0].toString());
+				poll.setPollTitle(objArr[1].toString());
+				poll.setEndAt(Timestamp.valueOf(objArr[2].toString()).toLocalDateTime());
+				
+				final Post post = new Post();
+				post.setId(objArr[3].toString());
+				poll.setPost(post);				
+				
+				poll.setIsActive(Boolean.valueOf(objArr[4].toString()));
+			}
+
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
+		Optional<Poll> pollOptional = Optional.ofNullable(poll);
+		return pollOptional;
+	}
+	
 }
--- lawencon-community/src/main/java/com/lawencon/community/dao/PollOptionDao.java
+++ lawencon-community/src/main/java/com/lawencon/community/dao/PollOptionDao.java
@@ -1,9 +1,42 @@
 package com.lawencon.community.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.lawencon.base.AbstractJpaDao;
+import com.lawencon.community.model.Poll;
+import com.lawencon.community.model.PollOption;
 
 @Repository
 public class PollOptionDao extends AbstractJpaDao {
 
+	public List<PollOption> getAllByPostId(final String pollId) {
+		final StringBuilder str = new StringBuilder();
+		str.append("SELECT p.id, p.poll_content, p.poll_id ").append("FROM t_poll p ")
+				.append("WHERE c.poll_id = :pollId ").append("ORDER BY c.id DESC");
+
+		final String sql = str.toString();
+		final List<?> result = createNativeQuery(sql).setParameter("pollId", pollId).getResultList();
+
+		final List<PollOption> pollOptions = new ArrayList<>();
+
+		if (result != null && result.size() > 0) {
+			result.forEach(checkInObj -> {
+				final Object[] objArr = (Object[]) checkInObj;
+				final PollOption pollOption = new PollOption();
+				pollOption.setId(objArr[0].toString());
+				pollOption.setPollContent(objArr[1].toString());
+				
+				final Poll poll = new Poll();
+				poll.setId(objArr[2].toString());
+				pollOption.setPoll(poll);
+
+				pollOptions.add(pollOption);
+			});
+		}
+
+		return pollOptions;
+	}
 }
