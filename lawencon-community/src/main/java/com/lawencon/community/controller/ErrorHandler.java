b10249155119a1cf4fdc2926bbae85b144f8499e
Muhammad Nur Fauzi <fauzi.ozi.mnf@gmail.com>
2022-11-18 17:41:56 +0700

    add working code verification with frontend

--- lawencon-community/src/main/java/com/lawencon/community/controller/ErrorHandler.java
+++ lawencon-community/src/main/java/com/lawencon/community/controller/ErrorHandler.java
@@ -6,11 +6,21 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.lawencon.community.dto.error.ErrorRes;
+import com.lawencon.util.VerificationCodeUtil.InvalidVerificationCodeException;
 
 @ControllerAdvice
 public class ErrorHandler {
 	@ExceptionHandler(RuntimeException.class)
 	public ResponseEntity<ErrorRes<String>> handleRuntime(final RuntimeException ex) {
+		final String error = ex.getMessage();
+		final ErrorRes<String> errorRes = new ErrorRes<>();
+		errorRes.setMessage(error);
+		ex.printStackTrace();
+		return new ResponseEntity<>(errorRes, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(InvalidVerificationCodeException.class)
+	public ResponseEntity<ErrorRes<String>> handleVerification(final InvalidVerificationCodeException ex){ 
 		final String error = ex.getMessage();
 		final ErrorRes<String> errorRes = new ErrorRes<>();
 		errorRes.setMessage(error);
