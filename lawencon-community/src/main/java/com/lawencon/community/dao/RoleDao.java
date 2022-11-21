70ece346e8f379e868fa66ef802156fdbb29c610
Angger Muhammad ELegant <anggerelegant7@gmail.com>
2022-11-21 10:49:41 +0700

    fix user dto and service, and implement register admin

--- lawencon-community/src/main/java/com/lawencon/community/dao/RoleDao.java
+++ lawencon-community/src/main/java/com/lawencon/community/dao/RoleDao.java
@@ -5,5 +5,23 @@ import com.lawencon.base.AbstractJpaDao;
 
 @Repository
 public class RoleDao extends AbstractJpaDao {
+	
+	public String getByRoleCode(final String roleCode) {
+		final StringBuilder str = new StringBuilder();
+		str.append("SELECT r.id ").append("FROM t_role r ")
+				.append("WHERE role_code = :roleCode ");
 
+		String roleId = null;
+		try {
+			final Object userObj = createNativeQuery(str.toString())
+					.setParameter("roleCode", roleCode).getSingleResult();
+			if (userObj != null) {
+				roleId = userObj.toString();
+			}
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
+		return roleId;
+	}
+	
 }
