517a1029d9e211bff94b16d6f7eeb26b0b934bfb
Angger Muhammad ELegant <anggerelegant7@gmail.com>
2022-11-22 11:29:17 +0700

    add and implement like service. renaming template files

--- lawencon-community/src/main/java/com/lawencon/community/controller/LikeController.java
+++ lawencon-community/src/main/java/com/lawencon/community/controller/LikeController.java
@@ -5,9 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lawencon.community.dto.InsertRes;
+import com.lawencon.community.dto.UpdateRes;
+import com.lawencon.community.dto.like.LikeInsertReq;
+import com.lawencon.community.dto.like.LikeRes;
+import com.lawencon.community.dto.like.LikeUpdateReq;
+import com.lawencon.community.dto.like.LikesRes;
 import com.lawencon.community.service.LikeService;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -23,5 +32,29 @@ public class LikeController {
 	public ResponseEntity<Long> countLike(@PathVariable("id") final String postId) {
 		final Long res = likeService.countLike(postId);
 		return new ResponseEntity<>(res, HttpStatus.OK);
+	}
+		
+	@GetMapping
+	public ResponseEntity<LikesRes> getAllByUserId() {
+		final LikesRes res = likeService.getAllByUserId();
+		return new ResponseEntity<>(res, HttpStatus.OK);
+	}
+
+	@PostMapping
+	public ResponseEntity<InsertRes> insert(@RequestBody final LikeInsertReq data) {
+		final InsertRes res = likeService.insert(data);
+		return new ResponseEntity<>(res, HttpStatus.OK);
+	}
+	
+	@PutMapping("soft-delete")
+	public ResponseEntity<UpdateRes> softDelete(@RequestBody final LikeUpdateReq data) {
+		final UpdateRes res = likeService.softDelete(data);
+		return new ResponseEntity<>(res, HttpStatus.OK);
+	}
+
+	@GetMapping("{id}")
+	public ResponseEntity<LikeRes> getById(@PathVariable("id") final String id) {
+		final LikeRes result = likeService.getById(id);
+		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 }
--- lawencon-community/src/main/java/com/lawencon/community/dao/LikeDao.java
+++ lawencon-community/src/main/java/com/lawencon/community/dao/LikeDao.java
@@ -1,8 +1,14 @@
 package com.lawencon.community.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.lawencon.base.AbstractJpaDao;
+import com.lawencon.community.model.Like;
+import com.lawencon.community.model.Post;
+import com.lawencon.community.model.User;
 
 @Repository
 public class LikeDao extends AbstractJpaDao {
@@ -23,4 +29,39 @@ public class LikeDao extends AbstractJpaDao {
 		}
 		return total;
 	}
+	
+	public List<Like> getAllByUserId(final String userId) {
+		final StringBuilder str = new StringBuilder();
+		str.append("SELECT l.id, l.ver, l.user_id, l.post_id, l.is_active ").append("FROM t_like l ")
+				.append("WHERE l.user_id = :userId ").append("AND l.is_active = TRUE ");
+
+		final String sql = str.toString();
+		final List<?> result = createNativeQuery(sql).setParameter("userId", userId).getResultList();
+
+		final List<Like> likes = new ArrayList<>();
+
+		if (result != null && result.size() > 0) {
+			result.forEach(resultObj -> {
+				final Object[] objArr = (Object[]) resultObj;
+				final Like like = new Like();
+				like.setId(objArr[0].toString());
+				like.setVersion(Integer.valueOf(objArr[1].toString()));
+
+				final User user = new User();
+				user.setId(objArr[2].toString());
+				like.setUser(user);
+
+				final Post post = new Post();
+				post.setId(objArr[3].toString());
+				like.setPost(post);
+
+				like.setIsActive(Boolean.valueOf(objArr[4].toString()));
+
+				likes.add(like);
+			});
+		}
+
+		return likes;
+	}
+
 }
